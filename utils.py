d0baab150d4558709d7753723ee7bb76ab9d1619
hannesk95 <hannes.kiechle@gmail.com>
2024-08-09 16:02:13 +0200

    add imbalance aware losses

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import mlflow
 import matplotlib.pyplot as plt
+from typing import Optional
+import torch.nn as nn
 
 from PIL import Image
 from torchvision import transforms
@@ -62,3 +64,147 @@ def create_confusion_matrix(y_true, y_pred):
 
     # return image_tensor, sensitivity, specificity
     return image, sensitivity, specificity
+
+class WeightedCombinedLosses(nn.Module):
+
+    def __init__(
+        self, losses: list[nn.Module], weights: Optional[list[float]] = None
+    ) -> None:
+        super().__init__()
+        self.losses = losses
+        # equal weights if not provided
+        self.weights = (
+            weights
+            if weights is not None
+            else [1 / len(self.losses)] * len(self.losses)
+        )
+
+    def forward(self, preds: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        loss = torch.zeros(1, device=preds.device)
+        for w, l in zip(self.weights, self.losses):
+            loss += w * l(preds, targets)
+
+        return loss
+    
+class SoftF1LossMinorityClass(torch.nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.soft_f1_loss_fn = SoftF1LossWithLogits()
+
+    def forward(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+
+        # flip targets such that minority class is 1
+        target = 1 - target
+        pred = -pred  # flip logits such that minority class is < 0
+
+        return self.soft_f1_loss_fn(pred, target)
+
+
+class SoftF1LossWithLogits(torch.nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+        pred = torch.sigmoid(pred)
+        tp = (target * pred).sum()
+        # tn = ((1 - targets) * (1 - preds)).sum()
+        fp = ((1 - target) * pred).sum()
+        fn = (target * (1 - pred)).sum()
+
+        p = tp / (tp + fp + 1e-16)
+        r = tp / (tp + fn + 1e-16)
+
+        soft_f1 = 2 * p * r / (p + r + 1e-16)
+
+        soft_f1 = soft_f1.mean()
+
+        return 1 - soft_f1
+
+
+class SoftF1LossMulti(torch.nn.Module):
+
+    def __init__(self, num_classes: int) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+        self.bin_loss_fn = SoftF1LossWithLogits()
+
+    def forward(
+        self,
+        pred: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+
+        loss = torch.zeros(1, device=pred.device, dtype=pred.dtype)
+        pred = torch.softmax(pred, dim=1)
+
+        for i in range(self.num_classes):
+            loss += self.bin_loss_fn(target[:, i], pred[:, i])
+
+        loss /= self.num_classes
+
+        return loss
+    
+
+# binary versions of the loss
+class SoftMCCLoss(nn.Module):
+
+    def forward(self, preds: torch.Tensor, labels: torch.Tensor):
+        tp = torch.sum(preds * labels)
+        tn = torch.sum((1 - preds) * (1 - labels))
+        fp = torch.sum(preds * (1 - labels))
+        fn = torch.sum((1 - preds) * labels)
+
+        numerator = tp * tn - fp * fn
+        denom = torch.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)) + 1e-8
+        soft_mcc = numerator / denom
+
+        loss = 1 - soft_mcc
+        return loss
+
+
+class SoftMCCWithLogitsLoss(SoftMCCLoss):
+
+    def forward(self, preds: torch.Tensor, labels: torch.Tensor):
+        preds_sigmoid = torch.sigmoid(preds)
+        return super().forward(preds_sigmoid, labels)
+
+
+# multi-class versions of the loss
+class SoftMCCLossMulti(nn.Module):
+    """With logits."""
+
+    def forward(self, preds: torch.Tensor, labels: torch.Tensor):
+        # create soft confusion matrix
+        preds = torch.softmax(preds, dim=1)
+
+        # total number of correct predictions, softened by the probability of each class
+        c = torch.sum(preds * labels)
+
+        # total number of samples
+        s = preds.size(0)
+
+        # number of times each class occured in the labels
+        t_k = torch.sum(labels, dim=0)
+
+        # number of times each class was predicted
+        p_k = torch.sum(preds, dim=0)
+
+        numerator = c * s - (t_k * p_k).sum()
+        denom = (
+            torch.sqrt(s**2 - p_k.square().sum())
+            * torch.sqrt(s**2 - t_k.square().sum())
+            + 1e-8
+        )
+
+        soft_mcc = numerator / denom
+        return 1 - soft_mcc
