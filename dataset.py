93b5ef0c5611fbc441e943c9640c103d402a0606
hannesk95 <hannes.kiechle@gmail.com>
2024-08-09 19:57:47 +0200

    add data augmentation

--- dataset.py
+++ dataset.py
@@ -1,10 +1,12 @@
 import os
 import torch
+import scipy
 import numpy as np
 
 from glob import glob
 from pathlib import Path
 from torch.utils.data import Dataset
+from scipy.ndimage import map_coordinates, gaussian_filter
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
@@ -102,8 +104,60 @@ class ResNetDataset(Dataset):
             case "regression":
                 label = torch.tensor(self.labels[idx]).to(torch.float32)
 
+        if self.split == "train":
+            if self.config.augmentation:
+                image = self.augment_image(image, apply_elastic=False)
+
         # for testing purposes only
         # image = torch.ones((1, 32, 32, 32))
         # label = torch.tensor(0).long()
 
         return image, label
+    
+
+    def random_flip(self, img):
+        if torch.rand(1).item() > 0.5:
+            img = torch.flip(img, dims=[0])  # Flip along the x-axis
+        if torch.rand(1).item() > 0.5:
+            img = torch.flip(img, dims=[1])  # Flip along the y-axis
+        if torch.rand(1).item() > 0.5:
+            img = torch.flip(img, dims=[2])  # Flip along the z-axis
+        return img
+    
+
+    def random_rotation(self, img):
+        angles = torch.rand(3) * 10  # Random angles between 0 and 20 degrees
+        img = scipy.ndimage.rotate(img, angles[0], axes=(1, 2), reshape=False)
+        img = scipy.ndimage.rotate(img, angles[1], axes=(0, 2), reshape=False)
+        img = scipy.ndimage.rotate(img, angles[2], axes=(0, 1), reshape=False)
+        return torch.tensor(img, dtype=torch.float32)
+    
+
+    def random_scaling(self, img, scale_range=(0.95, 1.05)):
+        scale = torch.rand(1).item() * (scale_range[1] - scale_range[0]) + scale_range[0]
+        img = scipy.ndimage.zoom(img, scale, order=1)
+        return torch.tensor(img, dtype=torch.float32)
+    
+    def elastic_deformation(self, img, alpha, sigma):
+        random_state = np.random.RandomState(None)
+        
+        shape = img.shape
+        dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+        dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+        dz = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
+        
+        x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]), indexing='ij')
+        indices = np.reshape(x + dx, (-1, 1)), np.reshape(y + dy, (-1, 1)), np.reshape(z + dz, (-1, 1))
+        
+        distored_image = map_coordinates(img, indices, order=1, mode='reflect')
+        distored_image = distored_image.reshape(img.shape)
+        
+        return torch.tensor(distored_image, dtype=torch.float32)
+    
+    def augment_image(self, img, apply_elastic=False):
+        # img = self.random_flip(img)
+        img = self.random_rotation(img)
+        img = self.random_scaling(img)
+        if apply_elastic:
+            img = self.elastic_deformation(img, alpha=30, sigma=3)
+        return img
